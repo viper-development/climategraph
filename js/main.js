d78ebc3bfcbb1e7039bcd90cd0c5c1db42f61963
Hemang Kumar <hemangsk@gmail.com>
2019-09-17 13:56:59 +0530

    Add predictions chart

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ loadJsonGraph('data/emissions_total/Germany.json', 'Total Emissions (in kt)', 'e
 
 loadJsonGraph('data/sector_sunburst.json', 'Emissions in tonnes per year from 2010', 'emissions-sector-sunburst');
 
+loadPredictionsCsv();
 
 function loadJsonGraph(url, title, id) {
   d3.json(url)
@@ -18,4 +19,78 @@ function loadJsonGraph(url, title, id) {
 function processData(data, title, id) {
   lineplot = document.getElementById(id);
 	Plotly.plot(lineplot, [data], {title: title, showlegend: true});
+}
+
+function loadPredictionsCsv() {
+  d3.csv('data/raw/predictions.csv')
+    .then((data) => {
+      let columns = Object.keys(data[0]);
+      columns.splice(0, 3);
+      let steps = [];
+      let frames = [];
+      flattened_data = {};
+      columns.forEach(column => {
+
+        flattened_data[column] = _.pluck(data, column);
+
+        steps.push({
+          name: column,
+          label: column,
+          method: 'animate',
+          args: [[column], {
+            mode: 'immediate',
+            frame: {redraw: false, duration: 500},
+            transition: {duration: 500}
+          }]
+        })
+
+        frames.push({
+          name: column,
+          data: [{
+            y: flattened_data[column],
+            'line.color': 'blue'
+          }]
+        })
+      })
+      plotGraph(steps, frames);
+    });
+}
+
+function plotGraph(steps, frames) {
+  predictionLinePlot = document.getElementById('prediction');
+  Plotly.plot(predictionLinePlot, {
+    data: [{
+      name: 'Projection',
+      x: d3.range(1990, 2100),
+      y: flattened_data['No climate policies (high)'],
+      mode: 'lines+markers',
+      line: {
+        color: 'blue',
+        simplify: false,
+      }
+    },
+    {
+      name: 'Historic',
+      x: d3.range(1990, 2100),
+      y: flattened_data['Historic'],
+      mode: 'lines+markers',
+      line: {
+        color: 'black',
+        simplify: false,
+      }
+    }],
+    layout: {
+      height: 700,
+      xaxis: {range: [1990, 2100]},
+      yaxis: {range: [-200, 200]},
+      sliders: [{
+        pad: {t: 30},
+        x: 0.05,
+        len: 0.95,
+        transition: {duration: 500},
+        steps
+      }],
+    },
+    frames
+  });
 }

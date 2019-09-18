b1d5417b6d39938481b27619d1635de2c26b3030
Hemang Kumar <hemangsk@gmail.com>
2019-09-19 00:18:01 +0530

    app: Arrange charts, update CSS

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-loadJsonGraph('data/emissions/World.json', 'Emissions Per Capita (Metric Tons)', 'emissions-per-capita');
-loadJsonGraph('data/emissions/Germany.json', 'Emissions Per Capita (Metric Tons)', 'emissions-per-capita');
-loadJsonGraph('data/emissions/China.json', 'Emissions Per Capita (Metric Tons)', 'emissions-per-capita');
-loadJsonGraph('data/emissions/Australia.json', 'Emissions Per Capita (Metric Tons)', 'emissions-per-capita');
+loadJsonGraph('data/emissions/World.json', 'emissions-per-capita');
+loadJsonGraph('data/emissions/Germany.json', 'emissions-per-capita');
+loadJsonGraph('data/emissions/China.json', 'emissions-per-capita');
+loadJsonGraph('data/emissions/Australia.json', 'emissions-per-capita');
 
-loadJsonGraph('data/emissions_total/Germany.json', 'Total Emissions (in kt)', 'emissions-total');
+loadJsonGraph('data/emissions_total/Germany.json', 'emissions-total');
 
-loadJsonGraph('data/sector_sunburst.json', 'Emissions in tonnes per year from 2010', 'emissions-sector-sunburst');
+loadJsonGraph('data/sector_sunburst.json', 'emissions-sector-sunburst');
 
 loadPredictionsCsv();
 
-function loadJsonGraph(url, title, id) {
+function loadJsonGraph(url, id) {
   d3.json(url)
     .then((data) => {
-      processData(data, title, id);
+      processData(data, id);
     })
 }
 
-function processData(data, title, id) {
+function processData(data, id) {
   lineplot = document.getElementById(id);
-	Plotly.plot(lineplot, [data], {title: title, showlegend: true});
+	Plotly.plot(lineplot, [data], { showlegend: true});
 }
 
 function loadPredictionsCsv() {
